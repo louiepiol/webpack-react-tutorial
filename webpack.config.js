65abc8542333ce0eef226741933c91507662aa77
Leryll Piol <leryll@centricitgroup.com>
2019-02-11 17:07:35 +0800

    Add node

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+
+const outputDirectory = 'dist';
 
 module.exports = {
+    entry: ['babel-polyfill', './src/client/index.js'],
+    output: {
+        path: path.join(__dirname, outputDirectory),
+        filename: 'bundle.js'
+    },
     module: {
       rules: [
         {
@@ -20,10 +29,17 @@ module.exports = {
         }
       ]
     },
+    devServer: {
+        port: 3000,
+        open: true,
+        proxy: {
+          '/api': 'http://localhost:8080'
+        }
+    },
     plugins: [
+        new CleanWebpackPlugin([outputDirectory]),
         new HtmlWebPackPlugin({
-          template: "./src/index.html",
-          filename: "./index.html"
+          template: "src/client/index.html"
         })
     ]
   };
