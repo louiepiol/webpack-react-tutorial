60d0dd7dfd1bba4b3762306c11491a206fafca13
Leryll Piol <leryll@centricitgroup.com>
2019-02-13 14:49:33 +0800

    Added webpack at dependency

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,2 @@
 import FormContainer from "./js/components/container/FormContainer.jsx";
+import '../dependencies/Semantic-UI-CSS-master/semantic.min.css';
--- webpack.config.js
+++ webpack.config.js
@@ -20,12 +20,20 @@ module.exports = {
           }
         },
         {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
           test: /\.html$/,
           use: [
             {
               loader: "html-loader"
             }
           ]
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          loader: 'url-loader?limit=100000'
         }
       ]
     },
